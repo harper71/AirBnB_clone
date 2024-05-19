afc252b6b15ef97c38f4b095eacb27a13be6a842
harper71 <princeazuka99@gmail.com>
2024-05-20 00:00:23 +0100

    console module

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""console of the clone app"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """command class to take in commands"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program
+
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the interpreter when Ctrl+D is pressed"""
+        return
+
+    def emptyline(self):
+        pass
+
+    def help(self):
+        """Quit command to exit the program"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
